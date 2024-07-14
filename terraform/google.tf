9188fd4f0ebd1061c079a2cf59c0f38e558de2dc
sbrupert <stevenbrupert@gmail.com>
2024-07-14 15:07:08 -0400

    Add comments to terraform resources for documentation.

--- terraform/google.tf
+++ terraform/google.tf
@@ -1,4 +1,5 @@
 # GCP Related Resources
+# This looks up the official Ubuntu 22 LTS GCP image so we can create our instance from it.
 data "google_compute_image" "ubuntu-22-lts" {
   family  = "ubuntu-2204-lts"
   project = "ubuntu-os-cloud"
@@ -8,7 +9,7 @@ data "google_compute_image" "ubuntu-22-lts" {
 resource "google_compute_instance" "web01" {
   machine_type = "e2-micro"
   name         = "web01"
-  zone = "us-east1-b"
+  zone = "us-east1-b" # Could also be "us-west1" or "us-central1" and be eligible for the free tier.
   labels = {
     instance_function = "webserver"
     env = "cloud-resume-prod"
@@ -17,7 +18,7 @@ resource "google_compute_instance" "web01" {
     auto_delete = true
     initialize_params {
       image = data.google_compute_image.ubuntu-22-lts.self_link
-      size  = 30
+      size  = 30 # Currently the largest disk we can use and stay within the free tier.
       type  = "pd-standard"
     }
 
@@ -30,6 +31,8 @@ resource "google_compute_instance" "web01" {
   scheduling {
     on_host_maintenance = "MIGRATE"
   }
+  # The metadata parameter is used to supply our user-data config file to Cloud-Init.
+  # This allows us to customize our instance with users, ssh keys, and more during it's creation.
   metadata = {
     user-data = templatefile("${path.module}/cloud-init.tftpl", {ssh_pub_keys = var.ssh_pub_keys})
   }
@@ -42,12 +45,18 @@ resource "google_compute_instance" "web01" {
 # Firestore Related Resources
 
 resource "google_firestore_database" "database" {
-  name        = "(default)"
+  name        = "(default)" # The firestore database must be named this to be counted by the free tier.
+  # There does not appear to be a restriction for where we can run the database. https://cloud.google.com/firestore/pricing
+  # However, we should keep it in the same region as our gcp instance for best performance.
   location_id = var.google_cloud_region
   type        = "FIRESTORE_NATIVE"
 }
 
 # IAM Related Resources
+# By using IAM resources, we can allow our Webserver to access the Firestore database without having to manage secrets.
+# NOTE: We could restrict our service account so the webserver would only be able to access it's own Firestore database.
+#       However, since IAM Service Accounts are scoped by project and we do not have any other databases in this project,
+#       the webserver won't be able to access any other database aside from it's own.
 resource "google_service_account" "webserver_sa" {
   account_id = "webserver"
   description = "Service account for webservers."
@@ -55,6 +64,8 @@ resource "google_service_account" "webserver_sa" {
 
 resource "google_project_iam_member" "webserver_db_access" {
   project = var.google_cloud_project
+  # Default GCP role that provides read/write access to a Datastore database. https://cloud.google.com/iam/docs/understanding-roles#datastore.user
+  # Even though it doeds not explicitly mention "Firestore", this is the correct role for our use case. 
   role    = "roles/datastore.user"
   member  = "serviceAccount:${google_service_account.webserver_sa.email}"
 }
@@ -78,6 +89,8 @@ resource "google_compute_firewall" "webserver_ssh_ingress" {
   description   = "Allow SSH traffic from anywhere to webservers."
 }
 
+# Note: Our ingress rules are overly permissive. This is fine if we aren't going to use Cloudflare and directly serve the site to visitors.
+#       But if our website will be behind Cloudflare's proxy, we should restrict access to Cloudflare-owned IPs: https://www.cloudflare.com/ips/       
 resource "google_compute_firewall" "webserver_http_ingress" {
   name    = "webserver-http-ingress"
   network = resource.google_compute_network.webserver_vpc.self_link
@@ -108,7 +121,7 @@ resource "google_compute_firewall" "webserver_egress" {
     protocol = "all"
   }
   # This way our webserver can access the package repositories and other resources it needs.
-  # If we could use URLs instead of IPs, then we could list all the domains we want to whitelist.
+  # If we could use URLs instead of IPs, then we could list all the domains we want to whitelist. However, this isn't supported by GCP at the moment.
   destination_ranges = ["0.0.0.0/0"]
   description        = "Allow webservers to reach anywhere."
 }
