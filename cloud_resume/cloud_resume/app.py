b52b1c29a5a975fa642a9c30f13d6c4cabe04906
Steven Rupert <stevenbrupert@gmail.com>
2024-10-24 23:01:50 -0400

    don't need to capture exceptions since increment_counter won't raise exceptions.

--- cloud_resume/cloud_resume/app.py
+++ cloud_resume/cloud_resume/app.py
@@ -12,13 +12,8 @@ app.wsgi_app = RequestLoggerMiddleware(app, app.wsgi_app)
 
 @app.route('/')
 def index():
-    try:
-        counter = increment_counter()
-        return render_template('index.html', counter=counter)
-    except Exception as e:
-        logger.error(f"Error while incrementing counter: {e}")
-        counter = "Unavailable"
-        return render_template('index.html', counter=counter)
+    counter = increment_counter()
+    return render_template('index.html', counter=counter)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False)
