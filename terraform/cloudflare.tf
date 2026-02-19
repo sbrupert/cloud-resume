44f60e85405324d3b7cb6316f84bc8e56cee1e7c
Steven Rupert <stevenbrupert@gmail.com>
2026-02-18 20:56:51 -0500

    Turn proxy on for cloudflare record.

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -4,7 +4,7 @@ resource "cloudflare_record" "cloud_resume" {
   # This dynamically sets the value of our DNS record to whatever the public IP of our GCP instance is.
   content = resource.google_compute_instance.web01.network_interface.0.access_config.0.nat_ip
   type = "A"
-  proxied = false
+  proxied = true
   # Prevents the record from being created before our GCP instance. Which would cause the run to fail.
   depends_on = [ google_compute_instance.web01 ]
 }
